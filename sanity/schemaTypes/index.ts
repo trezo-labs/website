8fd70cf77c825149d383e01fdcd1f318d4f7a502
“thelastofinusa” <thelastofinusa@gmail.com>
2026-04-09 17:48:17 +0100

    chore: remove Sanity CMS integration and related files
    Remove Sanity Studio, schema types, API routes, and client utilities as the CMS functionality is no longer needed. This includes deleting the admin page, configuration files, and removing related dependencies from package.json.

--- sanity/schemaTypes/index.ts
+++
@@ -1,9 +0,0 @@
-import { type SchemaTypeDefinition } from "sanity";
-
-import { presetType } from "./preset";
-import { templateType } from "./template";
-import { usageType } from "./cli-usage";
-
-export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [presetType, templateType, usageType],
-};
